a307e99472cca12a09e130e1e7cc8e7192481972
unknown <piotr.alejski@gmail.com>
2017-02-18 14:37:00 +0100

    Initial commit

---
+++ src/sda/Applocation.java
@@ -0,0 +1,10 @@
+package sda;
+
+/**
+ * Created by RENT on 2017-02-18.
+ */
+public class Applocation {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
